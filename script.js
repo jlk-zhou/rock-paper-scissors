5e49d35592fb5efe7f71370d7683a90928d989c9
Zach Joe <jlk.zhou@gmail.com>
2025-10-05 22:39:39 +0900

    Initial commit

---
+++ script.js
@@ -0,0 +1,59 @@
+let humanScore = 0; 
+let computerScore = 0; 
+
+/**
+ * Computer plays its hand in RPS. 
+ * Randomly returns one of "rock", "paper", and "scissors". 
+ * @returns {string} one of "rock", "paper", and "scissors" with equal chance. 
+ */
+function getComputerChoice() {
+    randomNumber = Math.random(); 
+    if (randomNumber <= (1 / 3)) {
+        return "rock"; 
+    } else if (randomNumber > (1 / 3) && randomNumber <= (2 / 3)) {
+        return "paper"; 
+    } else {
+        return "scissors"; 
+    }
+}
+
+/**
+ * Prompts user to type in their hand, then returns it. 
+ * The only legal inputs are "rock", "paper", "scissors", or their 
+ * capitalized variants. 
+ * This function assumes users will always input legal input. 
+ * @returns {string} The user's input. 
+ */
+function getHumanChoice() {
+    return prompt("Play it... ")
+}
+
+/**
+ * Lets human and computer play against each other, and increment score
+ * for whoever wins by one. 
+ * @param {string} humanChoice 
+ * @param {string} computerChoice 
+ */
+function playRound(humanChoice, computerChoice) {
+    humanChoice = humanChoice.toLowerCase(); 
+    if (humanChoice === computerChoice) {
+        console.log(`${humanChoice} vs ${computerChoice}: it's a tie! `); 
+    } else if ((humanChoice === "rock" && computerChoice === "paper")
+        || humanChoice === "paper" && computerChoice === "scissors" 
+        || humanChoice === "scissors" && computerChoice === "rock"
+    ) {
+        console.log(`You lose! ${computerChoice} beats ${humanChoice}. `); 
+        computerScore += 1; 
+    } else if (humanChoice === "rock" && computerChoice === "scissors"
+        || humanChoice === "paper" && computerChoice === "rock" 
+        || humanChoice === "scissors" && computerChoice === "paper"
+    ) {
+        console.log(`You win! ${humanChoice} beats ${computerChoice}. `); 
+        humanScore += 1; 
+    }
+}
+
+// const humanChoice = getHumanChoice (); 
+// const computerChoice = getComputerChoice(); 
+
+// playRound(humanChoice, computerChoice); 
